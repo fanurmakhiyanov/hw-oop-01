86d35ce58ca3f040bc0e184fa4d23514d2c9e840
fanur <makhiyanov.ff@gmail.com>
2022-11-25 17:11:20 +0300

    hw-oop-01

---
+++ src/main/Java/BottleVendingMachine.java
@@ -0,0 +1,7 @@
+import java.util.List;
+
+public class BottleVendingMachine extends VendingMachine{
+    public BottleVendingMachine(List<Product> list) {
+        super(list);
+    }
+}
--- src/main/Java/Main.java
+++ src/main/Java/Main.java
@@ -14,5 +14,17 @@ public class Main {
         VendingMachine vendingMachine = new VendingMachine(list);
         String key = "Twix";
         System.out.println(vendingMachine.getProductByName(key));
+
+        List<Product> listBottle = new ArrayList<Product>(
+                Arrays.asList(
+                        new Bottle("Coca-Cola", 50.0, 0.5),
+                        new Bottle("Sprite", 55.0, 0.5)
+                )
+        );
+
+        VendingMachine bottleMachine = new BottleVendingMachine(listBottle);
+        String keyBottle = "Sprite";
+        System.out.println(bottleMachine.getProductByName(keyBottle));
+
     }
 }
